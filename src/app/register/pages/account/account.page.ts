011ec3016f8eee6921f73306c1a444c0d1390723
Moitas500 <camilofour1@gmail.com>
2024-05-29 15:44:46 -0500

    Account page created

--- src/app/register/pages/account/account.page.ts
+++ src/app/register/pages/account/account.page.ts
@@ -1,13 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { 
+  IonImg,
+  IonInput,
+  IonList,
+  IonItem,
+  IonButton
+} from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-account',
   templateUrl: './account.page.html',
   styleUrls: ['./account.page.scss'],
   standalone: true,
-  imports: [CommonModule, FormsModule]
+  imports: [
+    CommonModule, 
+    FormsModule,
+    IonImg,
+    IonInput,
+    IonList,
+    IonItem,
+    IonButton
+  ]
 })
 export default class AccountPage implements OnInit {
 
--- src/app/register/register.page.ts
+++ src/app/register/register.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnDestroy, OnInit, WritableSignal, inject, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { NavigationEnd, Router } from '@angular/router';
 import { IonContent, 
   IonHeader, 
   IonTitle, 
@@ -12,8 +13,19 @@ import { IonContent,
   IonRouterOutlet,
   IonImg
 } from '@ionic/angular/standalone';
-import { NavigationEnd, Router } from '@angular/router';
 import { Subscription, filter } from 'rxjs';
+
+enum Titles {
+  CELLPHONE = 'NÚMERO CELULAR',
+  ACCOUNT = 'DATOS DE CUENTA',
+  END = 'FINALIZAR'
+}
+
+enum Routes {
+  CELLPHONE = '../../assets/imgs/form-1.svg',
+  ACCOUNT = '../../assets/imgs/form-2.svg',
+  END = '../../assets/imgs/form-3.svg'
+}
 
 @Component({
   selector: 'app-register',
@@ -38,23 +50,37 @@ import { Subscription, filter } from 'rxjs';
 export default class RegisterPage implements OnInit, OnDestroy{
 
   public routeSubscription!: Subscription
-  public title: WritableSignal<'NÚMERO CELULAR' | 'DATOS DE CUENTA' | 'FINALIZAR'> = signal('NÚMERO CELULAR')
-  public imgRoute: WritableSignal<string> = signal('../../assets/imgs/form-1.svg')
+  public title: WritableSignal<Titles> = signal(Titles.CELLPHONE)
+  public imgRoute: WritableSignal<Routes> = signal(Routes.CELLPHONE)
   private router = inject(Router)
 
   ngOnInit(): void {
-    this.routeSubscription = this.router.events.pipe(
-      filter(event => event instanceof NavigationEnd)
-    ).subscribe((event) => {
-      if (event instanceof NavigationEnd && event['url'] === '/register/account') {
-        this.title.set('DATOS DE CUENTA');
-        this.imgRoute.set('../../assets/imgs/form-2.svg');
-      }
-    })
+    this.subscribeToRoute()
   }
 
   ngOnDestroy(): void {
       this.routeSubscription.unsubscribe();
+  }
+
+  subscribeToRoute() {
+    this.routeSubscription = this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        switch(event['url']) {
+          case '/register/account':
+            this.updateItems(Titles.ACCOUNT, Routes.ACCOUNT)
+            break
+          case '/register/end':
+            this.updateItems(Titles.END, Routes.END)
+        }
+      }
+    })
+  }
+
+  updateItems(title: Titles, route: Routes) {
+    this.title.set(title);
+    this.imgRoute.set(route);
   }
 
 }
